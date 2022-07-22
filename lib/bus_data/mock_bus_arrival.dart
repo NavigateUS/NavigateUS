f40fdda851354473a875b08f087ba3afad9ab60c
hyc-g <haoyaochen00@gmail.com>
2022-07-22 20:02:45 +0800

    add basic mock bus arrival timing

--- lib/bus_data/mock_bus_arrival.dart
+++ lib/bus_data/mock_bus_arrival.dart
@@ -1,19 +1,30 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 
 Future<Response> getArrival(String stop, List<String> bus) async {
+
   final dio = Dio(BaseOptions());
   final dioAdapter = DioAdapter(dio: dio);
 
   const path = 'https://mocknusbus.com';
 
-  Map<String, String> res =  {'message': 'Success!'};
+  final String readResponse = await rootBundle.loadString('assets/bus_arrival.json');
+  final data = await json.decode(readResponse);
+
+  Map<String, String> arrivals = {};
+
+  for (String b in bus) {
+    arrivals[b] = data[stop][b]["next"];
+  }
+
 
   dioAdapter.onGet(
     path,
         (server) => server.reply(
-      200,
-      res,
+      200, arrivals,
     ),
   );
 
--- lib/widgets/detailed_directions_row.dart
+++ lib/widgets/detailed_directions_row.dart
@@ -1,9 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:navigateus/bus_data/bus_arrival.dart';
+import 'package:navigateus/bus_data/mock_bus_arrival.dart';
 
-class DetailedDirectionsRow extends StatelessWidget {
+class DetailedDirectionsRow extends StatefulWidget {
   final String stops;
   final String service;
   final int number;
@@ -19,14 +19,38 @@ class DetailedDirectionsRow extends StatelessWidget {
     required this.alight
   }) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() => DetailedDirectionsRowState();
+}
+
+class DetailedDirectionsRowState extends State<DetailedDirectionsRow>{
+  late Future<Response> arriveData;
+
+  @override
+  void initState() {
+    super.initState();
+
+    String service = super.widget.service;
+    String board = super.widget.board;
+
+    List<String> services = service.split('/');
+    arriveData = getArrival(board, services);
+  }
 
   @override
   Widget build(BuildContext context) {
+    String stops = super.widget.stops;
+    String service = super.widget.service;
+    int number = super.widget.number;
+    String board = super.widget.board;
+    String alight = super.widget.alight;
+
     List<String> services = service.split('/');
 
     return FutureBuilder<Response?>(
-      future: getArrival(board, services),
+      future: arriveData,
       builder: (context, snapshot) {
+        print(snapshot);
         if (snapshot.hasData) {
           Response data = snapshot.data!;
 
@@ -41,17 +65,30 @@ class DetailedDirectionsRow extends StatelessWidget {
 
           String text = '$number. Take bus $service at $board for $stopsTaken and alight at $alight';
 
-          String message = data.data['message'];
+          String result = "";
+
+          for (int i = 0; i < services.length; i++) {
+            String service = services[i];
+            result += "$service: ${data.data[service]}";
+            if (i != services.length - 1) {
+              result += "\n";
+            }
+          }
+
 
           return ListTile(
             title: Text(text),
-            subtitle: Text(message),
+            subtitle: Text(result),
+            onTap: () {super.setState(() {
+              arriveData = getArrival(board, services);
+            });},
           );
         }
         else {
-          return const CircularProgressIndicator();
+          return Container(height: 50, width: 50, padding: const EdgeInsets.all(10), child: const CircularProgressIndicator());
         }
       },
     );
   }
+
 }
